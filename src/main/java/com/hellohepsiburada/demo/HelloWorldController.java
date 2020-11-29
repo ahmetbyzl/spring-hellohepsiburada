1661a8ec1e364b86231172fa83aa8fcfd866af54
ahmetbyzl <ahmetbyzl@gmail.com>
2020-11-29 15:50:58 +0300

    project added

---
+++ src/main/java/com/hellohepsiburada/demo/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.hellohepsiburada.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+        @RequestMapping(value = "/", method = RequestMethod.GET)
+        @ResponseBody
+        public String helloWorld() {
+            return "Hello Hepsiburada from Ahmet Beyazoglu";
+
+        }
+
+}
---
+++ src/test/java/com/hellohepsiburada/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellohepsiburada.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
